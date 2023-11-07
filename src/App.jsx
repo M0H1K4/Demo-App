2bf6c9380b43b60b450e9ab5410697aec4e14231
M0H1K4N <nejer911@gmail.com>
2023-11-07 22:42:42 +0400

    adding Sidebar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Let's do It</h1>
-    </>
+    <main>
+     <ProjectsSidebar/>
+    </main>
   );
 }
 
