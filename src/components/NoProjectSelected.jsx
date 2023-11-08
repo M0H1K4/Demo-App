7b65e4a6a6620201675b44547c8cc3add5db193b
M0H1K4N <nejer911@gmail.com>
2023-11-08 18:15:20 +0400

    Add: new comp

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -6,9 +6,9 @@ export default function NoProjectSelected() {
     <div className="mt-2 text-center w-2/3">
       <img src={image} alt="Carieli" className="w-16 h-16 object-contain mx-auto" />
       <h2 className="text-xl font-bold text-stone-500 mt-4 my-4">No Projects Selected</h2>
-      <p className="text-stone-400">Select a project or get started with new one</p>
-      <p>
-        <button>Create new project</button>
+      <p className="text-stone-400 mb-4">Select a project or get started with new one</p>
+      <p className="mt-8">
+        <button className="">Create new project</button>
       </p>
     </div>
   );
