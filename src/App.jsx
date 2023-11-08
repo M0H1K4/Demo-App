4b1a2a1e4caad37458a669275c6aba836a492831
M0H1K4N <nejer911@gmail.com>
2023-11-08 17:53:34 +0400

    Add: NewProject comp To app js and Style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
      <ProjectsSidebar/>
      <NewProject/>
     </main>
