42f89141fef798a44989ad3da92b2e219c467868
M0H1K4N <nejer911@gmail.com>
2023-11-08 17:51:42 +0400

    New comp

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Input from "./Input";
 
 export default function NewProject() {
   return (
@@ -12,18 +13,9 @@ export default function NewProject() {
         </li>
       </menu>
       <div>
-        <p>
-          <label>Title</label>
-          <input type="text" />
-        </p>
-        <p>
-          <label>Description</label>
-          <textarea />
-        </p>
-        <p>
-          <label>Due Date</label>
-          <input type="text" />
-        </p>
+        <Input label="Title" />
+        <Input label="Description"  textarea/>
+        <Input label="Due Date" />
       </div>
     </div>
   );
