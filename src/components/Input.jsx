79594c7b5c39f1b9b9633ec002c4366207aa15ad
M0H1K4N <nejer911@gmail.com>
2023-11-08 18:03:47 +0400

    Add: styles to input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Input({ label, textarea, ...props }) {
   return (
-    <p>
-      <label>{label}</label>
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
       {textarea ? <textarea {...props} /> : <input {...props} />}
     </p>
   );
