0e326997c37666352f6c9151517421771c76d06c
M0H1K4N <nejer911@gmail.com>
2023-11-08 18:19:40 +0400

    Add: custom Button component

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import image from '../assets/no-projects.png';
+import Button from "./Button";
 
 export default function NoProjectSelected() {
   return (
@@ -8,7 +9,7 @@ export default function NoProjectSelected() {
       <h2 className="text-xl font-bold text-stone-500 mt-4 my-4">No Projects Selected</h2>
       <p className="text-stone-400 mb-4">Select a project or get started with new one</p>
       <p className="mt-8">
-        <button className="">Create new project</button>
+        <Button>Create new project</Button>
       </p>
     </div>
   );
