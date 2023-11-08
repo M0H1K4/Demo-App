2332f47e583894156788a9d2f9be25e12cd587ef
M0H1K4N <nejer911@gmail.com>
2023-11-08 18:02:30 +0400

    2

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Input({ label, textarea, ...props }) {
   return (
     <p>
-      <label></label>
+      <label>{label}</label>
       {textarea ? <textarea {...props} /> : <input {...props} />}
     </p>
   );
