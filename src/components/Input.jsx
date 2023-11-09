1c1800e5172417a714d9bfa3624d4c134c0980c1
M0H1K4N <nejer911@gmail.com>
2023-11-09 16:11:29 +0400

    Add: useRef in input componrnt

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
-export default function Input({ label, textarea, ...props }) {
- 
-  const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+const Input =  forwardRef(function Input({ label, textarea, ...props }, ref) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
 
   return (
     <p className="flex flex-col gap-1 my-4">
@@ -10,13 +10,12 @@ export default function Input({ label, textarea, ...props }) {
         {label}
       </label>
       {textarea ? (
-        <textarea
-          className={classes}
-          {...props}
-        />
+        <textarea className={classes} {...props} />
       ) : (
         <input className={classes} {...props} />
       )}
     </p>
   );
-}
+});
+
+export default Input;
