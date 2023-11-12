0094d03f72bdf4cc3ab1c96923709d4cb46d1459
M0H1K4N <nejer911@gmail.com>
2023-11-12 20:50:27 +0400

    Add

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Button from "./Button";
+import { list } from "postcss";
 
-export default function ProjectsSidebar({onStartAddProject}) {
+export default function ProjectsSidebar({ onStartAddProject, projects }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900  text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -10,6 +11,15 @@ export default function ProjectsSidebar({onStartAddProject}) {
       <div>
         <Button onClick={onStartAddProject}> +Add Project</Button>
       </div>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-2 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+              {project.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 }
